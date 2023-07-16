e65b71900a7c8cb83989817c8923f2b1ae35ba9b
Nirvic <rnirvic@gmail.com>
2023-07-16 11:13:17 +0545

    snake food and head

---
+++ js/index.js
@@ -0,0 +1,62 @@
+//Declaration and initialization
+let direction = {x: 0, y: 0};
+const foodSound = new Audio('food.mp3');
+const gameOverSound = new Audio('gameover.mp3');
+const moveSound = new Audio('move.mp3');
+const musicSound = new Audio('music.mp3');
+let speed = 2;
+let lastPaintTime = 0;
+let snakeArr = [
+    {x:13, y:15}
+]
+
+food ={x:6, y:7};
+
+alert("Red is head, Yellow is food")
+
+
+//Functions
+function main(ctime){  
+    window.requestAnimationFrame(main);
+    // console.log(ctime)
+    if ((ctime - lastPaintTime)/1000 < 1/speed){
+        return;
+    }
+    lastPaintTime = ctime;
+    gameEngine();
+}
+
+function gameEngine(){
+    //update
+
+
+    //display snake
+    board.innerHTML = "";
+    snakeArr.forEach((e, index)=>{
+        snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = e.y;
+        snakeElement.style.gridColumnStart = e.x;
+        if(index === 0){
+            snakeElement.classList.add('head');
+        }
+        else{
+            snakeElement.classList.add('snake');
+        }
+        board.appendChild(snakeElement);
+    })
+
+    //display food
+    foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add('food')
+    board.appendChild(foodElement);
+
+
+}
+
+
+
+
+//Main logic
+window.requestAnimationFrame(main);
